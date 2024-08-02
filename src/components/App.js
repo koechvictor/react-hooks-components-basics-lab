18d4c91073d49333a68051d654c154e4987e9bc3
koech victor <kipkiruiv408@gmail.com>
2024-08-02 06:04:18 +0000

    update

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,19 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+function About() {
+    return (
+        <h1>About</h1>
+    );
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
